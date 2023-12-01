934813e410e461b3ac10d0b5f2b249b04e12dd51
peppe-sc <giuseppe.scarso@hotmail.it>
2023-12-01 13:51:24 +0100

    0

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,37 @@
 import React, { useState } from 'react';
-
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 function NavBar(props){
+
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
     return(
         <>
+        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>BACKY</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'><a href="#">Home</a></li>
+        <li className='p-4'><a href="#">Company</a></li>
+        <li className='p-4'><a href="#">Resources</a></li>
+        <li className='p-4'><a href="#">About</a></li>
+        <li className='p-4'><a href="#">Contact</a></li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <a href="#"><AiOutlineClose size={20}/></a> : <a href="#"><AiOutlineMenu size={20} /></a>}
+      </div>
+      <ul  className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>BACKY</h1>
+        <li className='p-4 border-b border-gray-600'><a href="#">Home</a></li>
+        <li className='p-4 border-b border-gray-600'><a href="#">Company</a></li>
+        <li className='p-4 border-b border-gray-600'><a href="#">Resources</a></li>
+        <li className='p-4 border-b border-gray-600'><a href="#">About</a></li>
+        <li className='p-4'><a href="#">Contact</a></li>
+      </ul>
+    </div>
+        
         </>
     )
 }
---
+++ client/src/components/Presentation.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Presentation(){
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+          Build Your Own Backend
+        </h1>
+        <div className='flex justify-center items-center'>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>
+            Fast, Simple, Low-Code
+          </p>
+          
+        </div>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>Simply design your backend without writing any code</p>
+        <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default Presentation;
