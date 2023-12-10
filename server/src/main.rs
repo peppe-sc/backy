81c936a282831a793c03467b1cdddaaf00469dfa
peppe-sc <giuseppe.scarso@hotmail.it>
2023-12-10 22:26:48 +0100

    initial backend functions

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,17 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+mod db;
+
 #[get("/")]
 async fn hello() -> impl Responder {
+    
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/nodes")]
+async fn get_nodes() -> impl Responder{
+    let nodes = db::get_nodes();
+    HttpResponse::Ok().body(nodes.unwrap())
 }
 
 #[post("/echo")]
@@ -20,6 +29,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(get_nodes)
             .route("/hey", web::get().to(manual_hello))
             .route("/demo/{demo_id}",web::get().to(manual_hello))
     })
